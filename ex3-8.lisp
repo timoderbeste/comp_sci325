14afe7f2d1e4ada3a977de47fe19ad6b1480e68a
timoderbeste <ntwang1994@gmail.com>
2019-10-06 00:44:09 -0500

    Trying to solve definition way too long for ex3-8

--- ex3-8.lisp
+++ ex3-8.lisp
@@ -1,30 +1,35 @@
 (defun show-dots-helper (lst)
-  (cond ((and lst (atom lst)) (write-to-string lst))
-	((not (null lst)) (concatenate 'string "(" (show-dots-helper (car lst)) " . " (show-dots-helper (cdr lst)) ")" ))
+  (cond ((and lst (atom lst))
+	 (write-to-string lst))
+	((not (null lst))
+	 (concatenate 'string "(" (show-dots-helper (car lst)) " . " (show-dots-helper (cdr lst)) ")" ))
 	(t "NIL")
 	))
 
 (defun show-dots (lst)
   (format t (show-dots-helper lst)))
 
-(defun proper-list? (lst)
-  (or (null lst)
-      (and (consp lst)
-	   (proper-list? (cdr lst)))))
-
-
 (defun show-list-content (lst)
-  (cond ((and (atom (car lst)) (null (cdr lst)))
+  (cond ((and (null (cdr lst)) (atom (car lst)))
 	 (write-to-string (car lst)))
-	((and (atom (car lst)) (not (null (cdr lst))))
-	 (concatenate 'string (write-to-string (car lst)) " " (show-list-content (cdr lst))))
-	((and (not (atom (car lst))) (null (cdr lst)))
+	((and (null (cdr lst)) (consp (car lst)))
 	 (concatenate 'string "[" (show-list-content (car lst)) "]"))
-	((and (not (atom (car lst))) (not (null (cdr lst))))
-	 (concatenate 'string "[" (show-list-content (car lst)) "]" " " (show-list-content (cdr lst))))))
+	((and (atom (cdr lst)) (atom (car lst)))
+	 (concatenate 'string (write-to-string (car lst)) " . " (write-to-string (cdr lst))))
+	((and (atom (cdr lst)) (consp (car lst)))
+	 (concatenate 'string "[" (show-list-content (car lst)) "]" " . " (write-to-string (cdr lst))))	
+	((atom (car lst))
+	 (concatenate 'string (write-to-string (car lst)) " " (show-list-content (cdr lst))))
+	((consp (car lst))
+	 (concatenate 'string "[" (show-list-content (car lst)) "]" " " (show-list-content (cdr lst))))
+	(t "")))
 
 (defun show-list (lst)
   (if (atom lst)
       (format t (write-to-string lst))
-      (format t (concatenate 'string "[" (show-list-content lst) "]"))))      
+      (format t (concatenate 'string "[" (show-list-content lst) "]"))))
 
+
+
+
+      ;;;(format t (concatenate 'string "[" (show-list-content lst) "]"))))
