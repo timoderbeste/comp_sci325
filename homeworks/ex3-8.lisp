476bf9e7cd4df9ef41dc208b1f6091653993c836
timoderbeste <ntwang1994@gmail.com>
2019-10-11 00:46:53 -0500

    Correcting ex3-8

--- homeworks/ex3-8.lisp
+++ homeworks/ex3-8.lisp
@@ -1,18 +1,16 @@
 (defun show-dots (lst)
-  (cond ((and lst (atom lst))
-	 (format t "~A" lst))
-	((not (null lst))
+  (cond ((atom lst)
+         (format t "~S" lst))
+	(t
 	 (format t "(")
 	 (show-dots (car lst))
 	 (format t " . ")
 	 (show-dots (cdr lst))
-	 (format t ")"))
-	(t
-	 (format t "NIL"))))
+	 (format t ")"))))
 
 (defun show-list-proper (lst)
   (cond ((atom (car lst))
-	 (format t "~A" (car lst)))
+	 (format t "~S" (car lst)))
 	(t
 	 (format t "[")
 	 (show-list-full (car lst))
@@ -20,19 +18,19 @@
 
 (defun show-list-improper (lst)
   (cond ((atom (car lst))
-	 (format t "~A" (car lst))
+	 (format t "~S" (car lst))
 	 (format t " . ")
-	 (format t "~A" (cdr lst)))
+	 (format t "~S" (cdr lst)))
 	(t
 	 (format t "[")
 	 (show-list-full (car lst))
 	 (format t "]")
 	 (format t " . ")
-	 (format t "~A" (cdr lst)))))
+	 (format t "~S" (cdr lst)))))
 
 (defun show-list-rest (lst)
   (cond ((atom (car lst))
-	 (format t "~A " (car lst))
+	 (format t "~S " (car lst))
 	 (show-list-full (cdr lst)))
 	(t
 	 (format t "[")
@@ -50,7 +48,7 @@
 
 (defun show-list (lst)
   (cond ((atom lst)
-	 (format t "~A" lst))
+	 (format t "~S" lst))
 	(t
 	 (format t "[")
 	 (show-list-full lst)
