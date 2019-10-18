034861f8e437c7892036a678470b494cd3e29265
timoderbeste <ntwang1994@gmail.com>
2019-10-17 21:00:35 -0500

    Practicing shortest path

--- homeworks/ex3-2.lisp
+++
@@ -1,7 +0,0 @@
-(defun stable-union (set1 set2)
-  (cond ((null set2)
-	 set1)
-	((member (car set2) set1)
-	 (stable-union set1 (cdr set2)))
-	(t
-	 (stable-union (nconc set1 (list (car set2))) (cdr set2)))))
