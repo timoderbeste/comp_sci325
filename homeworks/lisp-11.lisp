7db567522aba43f976415dd6b34654531956157d
timoderbeste <ntwang1994@gmail.com>
2019-10-17 22:51:01 -0500

    Working on lisp 11

--- homeworks/lisp-11.lisp
+++ homeworks/lisp-11.lisp
@@ -1,20 +1,19 @@
 (defun map-range (func start end)
   (do ((i start (+ i (if (< start end) 1 -1)))
-       (results nil results))
-      ((= end i) (reverse results))
-    (push (funcall func i) results)))
+       (results nil (cons (funcall func i) results)))
+       ((= end i) (nreverse results))))
 
 (defun find-range (pred start end)
-  (do ((i start (+ i (if (< start end) 1 -1)))
-       (result nil (if (funcall pred i)
-		       i
-		       nil)))
-      ((or (= end i) (not (null result))) result)))
+  (do ((i start (+ i (if (< start end) 1 -1))))
+      ((or (= end i) (funcall pred i)) (if (= end i)
+					   nil
+					   i))))
 
 (defun every-range (pred start end)
-  (do ((i start (+ i (if (< start end) 1 -1)))
-       (result t (and result (funcall pred i))))
-      ((or (= end i) (null result)) result)))
+  (do ((i start (+ i (if (< start end) 1 -1))))
+      ((or (= end i) (not (funcall pred i))) (if (= end i)
+						 t
+						 nil))))
 
 (defun reduce-range (func start end &optional (init nil))
   (do ((i start (+ i (if (< start end) 1 -1)))
