b77ca9c3600bb237671c8560fc41aafe4bb20b35
timoderbeste <ntwang1994@gmail.com>
2019-10-17 23:15:52 -0500

    Working on lisp3

--- homeworks/lisp-3.lisp
+++ homeworks/lisp-3.lisp
@@ -1,6 +1,11 @@
-(defun make-balance (initial-balance)
+(defun make-balance-old (initial-balance)
   (let ((balance initial-balance))
     (lambda (&optional (update 0))
       (if (= update 0)
 	  balance
 	  (incf balance update)))))
+
+(defun make-balance (balance)
+  (lambda (&optional (update 0) (b balance))
+    (incf balance update)
+    balance))
