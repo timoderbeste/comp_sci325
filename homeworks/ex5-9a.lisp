4d632837d9078d34c0451d1b5f600cc9d6b1258b
timoderbeste <ntwang1994@gmail.com>
2019-10-24 18:52:31 -0500

    Fixing most critiques for ex5-9

--- homeworks/ex5-9a.lisp
+++ homeworks/ex5-9a.lisp
@@ -2,23 +2,23 @@
 (setf graph-w-cycle '((a b c) (b c) (c a) (d c)))
 
 (defun new-paths (path node net end)
-  (if (member end (cdr (assoc node net)))
-      (throw 'abort (reverse (cons end path)))
-      (mapcar #' (lambda (n)
-		   (cons n path))
-		 (remove-if (lambda (x) (member x path)) (cdr (assoc node net))))))
+  (mapcar (lambda (n)
+	      (cond ((member end (cdr (assoc node net)))
+		     (throw 'abort (reverse (cons end path))))
+		    ((not (member n path))
+		     (cons n path))
+		    (t nil)))
+	  (cdr (assoc node net))))
 
 (defun bfs (end queue net)
   (if (empty-queue-p queue)
       nil
       (let ((path (car queue)))
 	(let ((node (car path)))
-	  (if (eql node end)
-	      (reverse path)
 	      (bfs end
 		   (append (cdr queue)
 			   (new-paths path node net end))
-		   net))))))
+		   net)))))
 
 (defun shortest-path (start end net)
   (catch 'abort
