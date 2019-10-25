d54f471030736b2fbdb87c11d44cca881bf0533a
timoderbeste <ntwang1994@gmail.com>
2019-10-25 11:08:21 -0500

    Fixing most critiques for ex5-9

--- homeworks/ex5-9a.lisp
+++ homeworks/ex5-9a.lisp
@@ -2,23 +2,23 @@
 (setf graph-w-cycle '((a b c) (b c) (c a) (d c)))
 
 (defun new-paths (path node net end)
-  (mapcar (lambda (n)
+  (mapcan (lambda (n)
 	      (cond ((member end (cdr (assoc node net)))
 		     (throw 'abort (reverse (cons end path))))
 		    ((not (member n path))
-		     (cons n path))
+		     (list (cons n path)))
 		    (t nil)))
 	  (cdr (assoc node net))))
 
 (defun bfs (end queue net)
   (if (empty-queue-p queue)
       nil
-      (let ((path (car queue)))
-	(let ((node (car path)))
-	      (bfs end
-		   (append (cdr queue)
-			   (new-paths path node net end))
-		   net)))))
+      (let* ((path (car queue))
+	     (node (car path)))
+	(bfs end
+	     (append (cdr queue)
+		     (new-paths path node net end))
+	     net))))
 
 (defun shortest-path (start end net)
   (catch 'abort
--- homeworks/ex5-9b.lisp
+++ homeworks/ex5-9b.lisp
@@ -9,21 +9,21 @@
 				(remove-if (lambda (x) (member x path)) (cdr (assoc node net))))))))
 
 (defun new-paths (path node net)
-  (mapcar (lambda (n)
+  (mapcan (lambda (n)
 	    (unless (member n path)
-	      (cons n path)))
+	      (list (cons n path))))
 	  (cdr (assoc node net))))
 
 (defun bfs (end queue net)
   (if (empty-queue-p queue)
       nil
-      (let ((path (car queue)))
-	(let ((node (car path)))
-	  (if (member end (assoc node net))
-	      (reverse (cons end path))
-	      (let ((new-path (new-paths path node net)))
-		(bfs end
-		     (append (cdr queue) new-path) net)))))))
+      (let* ((path (car queue))
+	     (node (car path)))
+	(if (member end (assoc node net))
+	    (reverse (cons end path))
+	    (let ((new-path (new-paths path node net)))
+	      (bfs end
+		   (append (cdr queue) new-path) net))))))
 
 (defun shortest-path (start end net)
   (bfs end (list (list start)) net))
