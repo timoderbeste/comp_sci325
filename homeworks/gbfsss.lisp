d0dc1fd88d6afdd42cbcdf11ce56c6fef451fa8d
timoderbeste <ntwang1994@gmail.com>
2019-10-27 17:41:55 -0500

    Fixing critiques for gbfsss

--- homeworks/gbfsss.lisp
+++ homeworks/gbfsss.lisp
@@ -1,22 +1,24 @@
+(defun gen-new-paths (path gen)
+  (mapcan (lambda (s)
+	    (unless (member s path)
+	      (list (cons s path))))
+	  (cdr (funcall gen path))))
+
 (defun bfs (paths pred gen)
-  (if (empty-queue-p paths)
+  (if (null paths)
       nil
       (let* ((path (car paths))
 	     (state (car path)))
 	(if (funcall pred state)
 	    path
-	    (let ((new-path (funcall gen path)))
-	      (bfs (append (cdr paths) new-path) pred gen))))))
+	    (let ((new-paths (gen-new-paths path gen)))
+	      (bfs (append (cdr paths) new-paths) pred gen))))))
 
 (defun shortest-path (start end net)
   (let* ((pred (lambda (state)
 		(member end (assoc state net))))
-	(gen (lambda (path)
-	       (let ((node (car path)))
-		 (mapcar (lambda (n)
-			   (unless (member n path)
-			     (cons n path)))
-			 (cdr (assoc node net))))))
+	 (gen (lambda (path)
+		(assoc (car path) net)))
 	 (path (bfs (list (list start)) pred gen)))
     (if (null path)
 	nil
