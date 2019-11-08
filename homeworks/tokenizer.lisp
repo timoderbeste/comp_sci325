8d7bdf1c6cd7abd9e3af519fb43df73f8cd9b580
Timo Wang <timowang@dhcp-10-105-217-73.wireless.northwestern.private>
2019-11-08 13:18:16 -0600

    Fixing tokenizer. Needing to work more on extract token space because somehow next-token must be called twice to get to the next one.

--- homeworks/tokenizer.lisp
+++ homeworks/tokenizer.lisp
@@ -5,49 +5,50 @@
    (delim
     :initarg :delim
     :initform #\space
-    :accessor delim)))
+    :accessor delim)
+   (curr-index
+    :initarg :curr-index
+    :initform 0
+    :accessor curr-index)))
 
 (defun make-tokenizer (str &optional (delim #\space))
   (make-instance 'tokenizer :str str :delim delim))
 
-
-(defun next-index (tr-delim tr-str)
-  (let ((next-token-index (find-if-not (lambda (c) (equal c tr-delim)) tr-str)))
-    (if (null next-token-index)
+(defun next-index (tr-delim tr-str tr-curr-index)
+  (let ((next-token-init-char (find-if-not (lambda (c) (equal c tr-delim)) tr-str :start tr-curr-index)))
+    (if (null next-token-init-char)
 	nil
-	(search (format nil "~C" (find-if-not (lambda (c) (equal c tr-delim)) tr-str)) tr-str))))
+	(position next-token-init-char tr-str :start tr-curr-index))))
 
 (defmethod next-token-p ((tr tokenizer))
   (if (eql (delim tr) #\space)
-      (not (null (next-index (delim tr) (str tr))))
+      (not (null (next-index (delim tr) (str tr) (curr-index tr))))
       (not (null (str tr)))))
 
-(defun extract-token-space (tr-delim tr-str)
+(defun extract-token-space (tr-delim tr-str tr-curr-index)
   (if (or (= 0 (length tr-str)) (null tr-str))
       (values nil nil)
-      (let ((next-token-index (next-index tr-delim tr-str)))
+      (let ((next-token-index (next-index tr-delim tr-str tr-curr-index)))
 	(if (null next-token-index)
 	    (values nil nil)
-	    (let*((cutted-str (subseq tr-str (next-index tr-delim tr-str)))
-		  (next-delim-index (search (format nil "~C" tr-delim) cutted-str))
-		  (token (subseq cutted-str 0 next-delim-index))
-		  (rest-str (if (null next-delim-index) "" (subseq cutted-str next-delim-index))))
-	      (values token rest-str))))))
+	    (let*((next-delim-index (position tr-delim tr-str :start next-token-index))
+		  (token (subseq tr-str next-token-index next-delim-index)))
+	      (values token (1+ tr-curr-index)))))))
 
-(defun extract-token-other (tr-delim tr-str)
+(defun extract-token-other (tr-delim tr-str tr-curr-index)
   (if (= 0 (length tr-str))
       (values "" nil)
-      (let ((next-delim-index (search (format nil "~C" tr-delim) tr-str)))
+      (let ((next-delim-index (position tr-delim tr-str)))
 	(if (null next-delim-index)
 	    (values tr-str nil)
 	    (values (subseq tr-str 0 next-delim-index) (subseq tr-str (1+ next-delim-index)))))))
 
 (defmethod next-token ((tr tokenizer))
   (if (eql #\space (delim tr))
-      (multiple-value-bind (token rest-str) (extract-token-space (delim tr) (str tr))
-	(setf (str tr) rest-str)
+      (multiple-value-bind (token next-index-value) (extract-token-space (delim tr) (str tr) (curr-index tr))
+	(setf (curr-index tr) next-index-value)
 	token)
-      (multiple-value-bind (token rest-str) (extract-token-other (delim tr) (str tr))
+      (multiple-value-bind (token rest-str) (extract-token-other (delim tr) (str tr) (curr-index tr))
 	(setf (str tr) rest-str)
 	token)))
 
