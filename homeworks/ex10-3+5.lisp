d8320594e80b3557a2a22a9a3b21430484bc0495
Ning Wang <nwang16@illinois.edu>
2019-11-17 22:41:59 -0600

    Adding ex10-3+5

---
+++ homeworks/ex10-3+5.lisp
@@ -0,0 +1,15 @@
+;;; I am mimicking random-choice.. the original mapcar seems to evalue all exprs so I set a condition and only evaluate if i == n-1. not sure if this is "cheating"...
+(defmacro nth-expr (n &rest exprs)
+  `(case (1- ,n)
+	 ,@(let ((i -1))
+		 (mapcar #'(lambda(expr) `(,(incf i) (if `(= i (1- n)) ,expr nil))) exprs))))
+
+
+(defmacro n-of (n expr)
+  (let ((g-n (gensym))
+		(g-i (gensym))
+		(g-lst (gensym)))
+	`(do ((,g-i 0 (1+ ,g-i))
+		  (,g-n ,n)
+		  (,g-lst nil (cons ,expr ,g-lst)))
+		 ((= ,g-i ,g-n) (nreverse ,g-lst)))))
