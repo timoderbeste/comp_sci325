4d66d3f78a7185d3712e70d53e92f7904820eff9
timoderbeste <ntwang1994@gmail.com>
2019-11-21 19:38:23 -0600

    FIxing tokenizer

--- homeworks/tokenizer.lisp
+++ homeworks/tokenizer.lisp
@@ -13,10 +13,10 @@
 
 (defun next-token-start (delim str curr-index)
   (cond ((not curr-index) nil)
-	((and (= curr-index (length str)) (not (eql delim #\space))) curr-index)
-	((and (< curr-index (length str)) (eql #\space delim))
-	 (position delim str :test-not #'equal :start curr-index))
-	((< curr-index (length str)) curr-index)
+	((and (= (1+ curr-index) (1+ (length str))) (not (eql delim #\space))) curr-index)
+	((and (< (1+ curr-index) (1+ (length str))) (eql #\space delim))
+	 (position delim str :test-not #'equal :start (1+ curr-index)))
+	((< (1+ curr-index) (1+ (length str))) (1+ curr-index))
 	(t nil)))
 
 (defun next-token-end (delim str start)
@@ -24,10 +24,10 @@
 
 
 (defun make-tokenizer (str &optional (delim #\space))
-  (make-instance 'tokenizer :str str :delim delim :curr-index (if (eql delim #\space) (next-token-start delim str 0) 0)))
+  (make-instance 'tokenizer :str str :delim delim :curr-index  -1))
 
 (defmethod next-token-p ((tr tokenizer))
-  (curr-index tr))
+  (next-token-start (delim tr) (str tr) (curr-index tr)))
 
 (defmethod next-token (tr)
   (cond ((and (not (next-token-p tr)) (eql (delim tr) #\space)) nil)
@@ -35,5 +35,5 @@
 	(t
 	 (let* ((start (next-token-start (delim tr) (str tr) (curr-index tr)))
 		(end (next-token-end (delim tr) (str tr) start)))
-	   (setf (curr-index tr) (next-token-start (delim tr) (str tr) (and end (1+ end))))
+	   (setf (curr-index tr) end)
 	   (subseq (str tr) start end)))))
