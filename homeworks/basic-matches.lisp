7d0648bc24669e893fc51e8f8465d6ecb0d2bb7f
Timo Wang <ntwang1994@gmail.com>
2019-11-22 00:52:00 -0600

    Finshing ?or

--- homeworks/basic-matches.lisp
+++ homeworks/basic-matches.lisp
@@ -1,7 +1,13 @@
 (in-package #:exmatch)
 
-(defun ?or (xs ys lsts))
+(defun ?or (xs y lsts)
+  (cond ((null lsts)
+	 nil)
+	 ((null xs)
+	 nil)
+	 (t (append (match-p (car xs) y lsts)
+		    (?or (cdr xs) y lsts)))))
 
-(defun ?not (xs ys lsts))
+(defun ?not (xs y lsts))
 
-(defun ?= (xs ys lsts))
+(defun ?= (xs y lsts))
