8ab5d8888c343a75bcc3081ac801b9ee424122f2
Timo Wang <ntwang1994@gmail.com>
2019-11-27 19:48:44 +0100

    Fixing basic-matches

--- homeworks/basic-matches.lisp
+++ homeworks/basic-matches.lisp
@@ -6,16 +6,15 @@
 	(t (append (match-p (car xs) y lsts)
 		   (?or (cdr xs) y lsts)))))
 
+;;; Not sure if this counts as mouse tail? I am trying to make it clear the parts for functional calls.
+
 (defun ?not (x y lsts)
   (cond ((null lsts) nil)
 	((null x) nil)
-	(t (append (if (match-p (car x) y (list (car lsts)))
-		       nil
-		       (list (car lsts)))
-		   (?not x y (cdr lsts))))))
+	(t (remove-if #'(lambda (lst)
+			  (match-p (car x) y (list lst)))
+		      lsts))))
 
 (defun ?= (xs y lsts)
-  (let ((sub-pattern (car xs))
-	(function-name (cadr xs))
-	(args (cddr xs)))
-    (match-p sub-pattern (apply function-name (cons y args)) lsts)))
+  (destructuring-bind (sub-pattern function-name . args) xs
+    (match-p sub-pattern (apply function-name (cons y args)) lsts)))    
