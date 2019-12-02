03abceed21fe901e874196f7d7690ac7c51eb093
Timo Wang <ntwang1994@gmail.com>
2019-12-02 11:53:17 -0600

    Working on make-best-change

--- homeworks/make-best-change.lisp
+++ homeworks/make-best-change.lisp
@@ -1,15 +1,30 @@
 ;;;(defun make-best-change-helper (sum coin-types comb-to-count))
 
-(defun make-best-change (sum &optional (coin-types '(25 10 5 1)))
+(defun make-best-change-helper (sum coin-types)
   (do ((rest-coin-types coin-types (cdr rest-coin-types))
        (curr-best-comb nil
 		       (let ((curr-coin-type (car rest-coin-types)))
 			 (if (> curr-coin-type sum)
 			     curr-best-comb
 			     (let ((curr-comb (cons curr-coin-type
-						    (make-best-change (- sum curr-coin-type) coin-types))))
-			       ;;;(print curr-comb)
+						    (make-best-change-helper (- sum curr-coin-type) coin-types))))
 			       (if (or (null curr-best-comb) (> (length curr-best-comb) (length curr-comb)))
 				   curr-comb
 				   curr-best-comb))))))
       ((null rest-coin-types) curr-best-comb)))
+
+
+(defun count-coins (best-comb coin-types)
+  (do ((rest-coin-types coin-types (cdr rest-coin-types))
+       (type-to-count nil (cons (cons (car rest-coin-types) 0) type-to-count)))
+      ((null rest-coin-types)
+       (do ((rest-comb best-comb (cdr rest-comb)))
+	   ((null rest-comb)
+	    (do ((rest-type-to-count type-to-count (cdr rest-type-to-count))
+		 (counts nil (cons (cdar rest-type-to-count) counts)))
+		((null rest-type-to-count) (values-list counts))))
+	 (setf (cdr (assoc (car rest-comb) type-to-count)) (1+ (cdr (assoc (car rest-comb) type-to-count))))))))
+
+
+(defun make-best-change (sum &optional (coin-types '(25 10 5 1)))
+  (count-coins (make-best-change-helper sum coin-types) coin-types))
